9eac5d38e3d18264604b8e7cbbb18294ce9414dd
Cyiox <cyioxox@gmail.com>
2026-08-12 13:54:19 -0400

    Added .gitignore file

--- obj/Debug/net10.0/WebPresDB.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebPresDB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebPresDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f45b3745439f62c539cceab0da0a0dc6e53b2b79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a96b55047820ed816a6e2b02d42fc4f4fba8f34e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebPresDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebPresDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
